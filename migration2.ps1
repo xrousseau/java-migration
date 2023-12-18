8579fe13126bf0377ab42d18e9bd99fcc450de2c
drousseau <xrousseau@gmail.com>
2023-12-18 20:22:53 +0000

    refactor for clarity

--- migration2.ps1
+++ migration2.ps1
@@ -21,65 +21,80 @@ $openJDKPaths = @{
     "jdk-11" = "C:\Program Files\Eclipse Adoptium\jdk-11.0.21.9-hotspot"
     "jdk-17" = "C:\Program Files\Eclipse Adoptium\jdk-17.0.9_9-hotspot"
     "jdk-21" = "C:\Program Files\Eclipse Adoptium\jdk-21.0.1_12-hotspot"
-} 
+}
 
+function Get-OracleJavaInstalledVersion {
+    # Retreive Oracle Java paths from registry to later reconstruct with junction path after uninstallation
+    $registryPath = "HKLM:\SOFTWARE\JavaSoft"
+    $javaHomeValues = @()
 
-# Retreive Oracle Java paths from registry to later reconstruct with junction path after uninstallation
-$registryPath = "HKLM:\SOFTWARE\JavaSoft"
-$javaHomeValues = @()
-$openJDKPackagesToInstall = @()
+    $javaHomeEntries = Get-ChildItem -Path $registryPath -Recurse | Get-ItemProperty | Where-Object { $_.PSObject.Properties.Name -contains 'JavaHome' }
 
-$javaHomeEntries = Get-ChildItem -Path $registryPath -Recurse | Get-ItemProperty | Where-Object { $_.PSObject.Properties.Name -contains 'JavaHome' }
+    foreach ($entry in $javaHomeEntries) {
+        $javaHomeValue = $entry.JavaHome
+        if ($javaHomeValue -notin $javaHomeValues) {
+            $javaHomeValues += $javaHomeValue
+            Write-Output "Javapath found -> $javaHomeValue" 
+        }
+    }
 
-foreach ($entry in $javaHomeEntries) {
-    $javaHomeValue = $entry.JavaHome
-    if ($javaHomeValue -notin $javaHomeValues) {
-        $javaHomeValues += $javaHomeValue
+    # Find the default Java version installed
+    $defaultVersion = (Get-Command java).Version.Major
+    Write-Output "Default Java is versio $defaultVersion" 
+
+    # Move defaut version at the end of the Array
+    $index = ($javaHomeValues | Where-Object {$_ -match "\\(jdk|jre)-$defaultVersion"} | Select-Object -Last 1)
+
+    if ($null -ne $index) {
+        $javaHomeValues = [System.Collections.ArrayList]$javaHomeValues
+        $javaHomeValues.Remove($index)
+        $javaHomeValues.Add($index)
+    }
+
+    # Save to file for recovery needs
+    $javaHomeValues | Out-File -FilePath ".\javapath.txt"
+    Write-Output "Data saved in .\javapath.txt " 
+}
+
+function Install-OpenJDK {
+    # Install OpenJDK equivalent version to Oracle Java  
+    $javaHomeValues = Get-Content -Path ".\javapath.txt"
+    $downloadsPath = (New-Object -ComObject Shell.Application).NameSpace('shell:Downloads').Self.Path
+
+    foreach ($javaHomeValue in $javaHomeValues) { 
+        $version = JavaVersion -Path $javaHomeValue
+        $openJDKPackagesToInstall = $downloadsPath + "\" + $jrePackages[$version]
+        Write-Host "Installing $openJDKPackagesToInstall"
+        $installCmd = "/i ""$($openJDKPackagesToInstall)"" RebootYesNo=No /q"
+        Start-Process -FilePath "msiexec.exe" -ArgumentList $installCmd -Wait
+        Write-Host "$openJDKPackagesToInstall installed"
     }
 }
 
-# Find the default Java version installed
-$version = (Get-Command java).Version.Major
-
-# Move defaut version at the end of the Array
-$index = ($javaHomeValues | Where-Object {$_ -match "\\(jdk|jre)-$version"} | Select-Object -Last 1)
-
-if ($null -ne $index) {
-    $javaHomeValues = [System.Collections.ArrayList]$javaHomeValues
-    $javaHomeValues.Remove($index)
-    $javaHomeValues.Add($index)
+function Uninstall-OracleJava {
+    # Uninstall all Oracle Java instances
+    $guidsArray = Get-CimInstance -ClassName Win32_Product | Where-Object { $_.Vendor -like "*Oracle*" -and $_.Name -like "*Java*" } | ForEach-Object { $_.IdentifyingNumber }
+    foreach ($guid in $guidsArray) {
+        Write-Host "Attempting removal of $guid "
+        $uninstallCmd = "/x `"$guid`" /qn"
+        Start-Process -FilePath "msiexec.exe" -ArgumentList $uninstallCmd -Wait -NoNewWindow
+    }
 }
 
-# Save to file for recovery needs
-$javaHomeValues | Out-File -FilePath ".\javapath.txt"
+function New-JunctionPaths {
+    # Create Junction Path to mock previous Oracle installation
+    $javaHomeValues = Get-Content -Path ".\javapath.txt"
 
-# Install OpenJDK equivalent version to Oracle Java  
-$javaHomeValues = Get-Content -Path ".\javapath.txt"
-$downloadsPath = (New-Object -ComObject Shell.Application).NameSpace('shell:Downloads').Self.Path
+    foreach ($javaHomeValue in $javaHomeValues) {
 
-foreach ($javaHomeValue in $javaHomeValues) { 
-    $version = JavaVersion -Path $javaHomeValue
-    $openJDKPackagesToInstall = $downloadsPath + "\" + $jrePackages[$version]
-    Write-Host "Installing $openJDKPackagesToInstall"
-    $installCmd = "/i ""$($openJDKPackagesToInstall)"" RebootYesNo=No /q"
-    Start-Process -FilePath "msiexec.exe" -ArgumentList $installCmd -Wait
-    Write-Host "$openJDKPackagesToInstall installed"
+        $version = JavaVersion -Path $javaHomeValue
+        $openJdkPath = $openJDKPaths[$version]
+        Write-Output "Creating Junction Path: $javaHomeValue -> $openJdkPath"
+        New-Item -ItemType Junction -Path $javaHomeValue -Target $openJdkPath
+    }
 }
 
-# Uninstall all Oracle Java instances
-$guidsArray = Get-CimInstance -ClassName Win32_Product | Where-Object { $_.Vendor -like "*Oracle*" -and $_.Name -like "*Java*" } | ForEach-Object { $_.IdentifyingNumber }
-foreach ($guid in $guidsArray) {
-    Write-Host "Attempting removal of $guid "
-    $uninstallCmd = "/x `"$guid`" /qn"
-    Start-Process -FilePath "msiexec.exe" -ArgumentList $uninstallCmd -Wait -NoNewWindow
-}
-
-# Create Junction Path to mock previous Oracle installation
-$javaHomeValues = Get-Content -Path ".\javapath.txt"
-
-foreach ($javaHomeValue in $javaHomeValues) {
-
-    $version = JavaVersion -Path $javaHomeValue
-    $openJdkPath = $openJDKPaths[$version]
-    New-Item -ItemType Junction -Path $javaHomeValue -Target $openJdkPath
-}
+Get-OracleJavaInstalledVersion
+Install-OpenJDK
+Uninstall-OracleJava
+New-JunctionPaths
